f92d6fe47c85e64f5f6f388430c5a03b2f671daf
seobaeksol <seobaeksol@gmail.com>
2025-12-11 00:10:24 +0900

    [concurrency-mandelbrot] write png file

--- concurrency-mandelbrot/src/main.rs
+++ concurrency-mandelbrot/src/main.rs
@@ -121,6 +121,40 @@ fn render(
         }
     }
 }
+
+use image::{ColorType, ImageError};
+
+/// It saves the `pixel` buffer of the `bounds` size as `filename`
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), ImageError> {
+    image::save_buffer(
+        filename,
+        pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::L8,
+    )
+}
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprint!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprint!(
+            "Exapmle: {} mandel.png 1000x750 -1.20,0.35 -1.0,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
