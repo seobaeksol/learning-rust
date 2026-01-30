6dfeca7871925ad5ba09134236c08ce69c9012b3
seobaeksol <seobaeksol@gmail.com>
2026-01-30 16:59:51 +0900

    [async] add a simple comparison between async and thread

---
+++ async/concurrency/src/main.rs
@@ -0,0 +1,44 @@
+use std::time::Duration;
+
+use trpl;
+
+fn main() {
+    println!("[Test Thread]");
+    trpl::block_on(test_thread());
+    println!("[Test Future]");
+    trpl::block_on(test_future());
+}
+
+async fn test_thread() {
+    let handle = trpl::spawn_task(async {
+        for i in 1..10 {
+            println!("hi number {i} from the first task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the second task!");
+        trpl::sleep(Duration::from_millis(500)).await;
+    }
+
+    handle.await.unwrap();
+}
+
+async fn test_future() {
+    let fut1 = async {
+        for i in 1..10 {
+            println!("hi number {i} from the first task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+    };
+
+    let fut2 = async {
+        for i in 1..10 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+    };
+
+    trpl::join(fut1, fut2).await;
+}
